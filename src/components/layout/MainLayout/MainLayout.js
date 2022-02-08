17a606374f42ac62217485618e0d32aee9251608
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-02-09 00:26:07 +0100

    Add change login status

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
-import { PageNav } from '../PageNav/PageNav';
+import { updateLoginStatus } from '../../../redux/loginRedux';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
 
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './MainLayout.module.scss';
@@ -17,7 +18,11 @@ const Component = ({className, children}) => (
     <AppBar>
       <Container maxWidth='lg'>
         <Toolbar disableGutters>
-          <PageNav />
+          <select onChange={(e) => updateLoginStatus(e.target.value)} >
+            <option value="login">Login</option>
+            <option value="logout">Logout</option>
+            <option value="admin">admin</option>
+          </select>
         </Toolbar>
       </Container>
     </AppBar>
@@ -31,20 +36,23 @@ const Component = ({className, children}) => (
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  updateLoginStatus: PropTypes.func,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const mapDispatchToProps = dispatch => ({
+  updateLoginStatus: log => dispatch(updateLoginStatus(log)),
+});
+
+const ReduxContainer = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as MainLayout,
-  // Container as MainLayout,
+  ReduxContainer as MainLayout,
   Component as MainLayoutComponent,
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { initialState } from './initialState';
 import { reducer as postsReducer } from './postsRedux';
+import { reducer as loginReducer } from './loginRedux';
 
 // define reducers
 const reducers = {
   posts: postsReducer,
+  isLogged: loginReducer,
 };
 
 // add blank reducers for initial state properties without reducers
