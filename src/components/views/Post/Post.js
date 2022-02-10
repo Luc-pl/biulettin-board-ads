c48af2e798fc826b9dda50dfe278dae88e3adc47
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-02-11 00:25:23 +0100

    Add Post view

--- src/components/views/Post/Post.js
+++ src/components/views/Post/Post.js
@@ -1,37 +1,72 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import clsx from 'clsx';
-
-// import { connect } from 'react-redux';
+//import clsx from 'clsx';
+import { connect } from 'react-redux';
+import { getAllPosts } from '../../../redux/postsRedux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Post.module.scss';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Post</h2>
-    {children}
-  </div>
-);
+const Component = ({posts, match}) => {
+
+  const post = posts.find(el => el.id === match.params.id);
+  const { title, image, imageTitle, description, publicationDate, location, price } = post;
+
+  return (
+    <Container className={styles.cardGrid} maxWidth="md">
+      <Grid item>
+        <Card className={styles.card}>
+          <CardMedia
+            className={styles.cardMedia}
+            image={image}
+            title={imageTitle}
+          />
+          <CardContent className={styles.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography>
+              {description}
+            </Typography>
+            <Typography className={styles.cardInfo}>
+              {`${location} - ${publicationDate}`}
+            </Typography>
+            <Typography className={styles.cardPrice}>
+              {`Price: ${price}$`}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Container>
+  );
+};
 
 Component.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string,
+  posts: PropTypes.array,
+  match: PropTypes.object,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  posts: getAllPosts(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const ReduxContainer = connect(mapStateToProps)(Component);
 
 export {
-  Component as Post,
-  // Container as Post,
+  //Component as Post,
+  ReduxContainer as Post,
   Component as PostComponent,
 };
